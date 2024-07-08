e7f292974d42e42972fc268dc611526ce1eba963
andriruslam <andri.wahyu@accelbyte.net>
2024-07-08 20:49:56 +0700

    fix: update error message

--- main.go
+++ main.go
@@ -16,40 +16,55 @@ import (
 	3. How to save data to CSV file.
 */
 
-func readReports(reportDir string) [][]string {
+func readReports(reportDir string) ([][]string, error) {
 	file, err := os.Open(reportDir)
 
 	if err != nil {
-		fmt.Errorf("error while reading the file: %s", err)
-		return nil
+		errMsg := fmt.Errorf("error while reading the file: %s", err)
+		return nil, errMsg
 	}
 
 	reader := csv.NewReader(file)
 	records, err := reader.ReadAll()
 
 	if err != nil {
-		fmt.Errorf("error reading records: %s", err)
-		return nil
+		errMsg := fmt.Errorf("error reading records: %s", err)
+		return nil, errMsg
 	}
 
-	defer file.Close()
+	defer func(file *os.File) {
+		err = file.Close()
+		if err != nil {
+			return
+		}
+	}(file)
 
-	return records
+	return records, nil
 }
 
-func writeReports(targetDir string, records [][]string) {
+func writeReports(targetDir string, records [][]string) error {
 	file, err := os.Create(fmt.Sprintf("%s.csv", targetDir))
 	if err != nil {
-		fmt.Errorf("failure to write file: %s", err)
-		return
+		errMsg := fmt.Errorf("failure to write file: %s", err)
+		return errMsg
 	}
 
-	defer file.Close()
+	defer func(file *os.File) {
+		err = file.Close()
+		if err != nil {
+			return
+		}
+	}(file)
 
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
 
-	writer.WriteAll(records)
+	err = writer.WriteAll(records)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // checking time range whether it's between the time range or not.
@@ -73,7 +88,7 @@ func checkTimeRange(start, end, check time.Time) string {
 // search which file that contains value from the searched date. the search algorithm using binary search.
 // the output are int and string value. int represent the file identifier where the search process step.
 // string represent the end result, whether the searched date is in the file ('equal') or in another file ('left' or 'right')
-func searchFile(mainDir string, totalFiles int, searchedDate time.Time) (int, string) {
+func searchFile(mainDir string, totalFiles int, searchedDate time.Time) (int, string, error) {
 	minRange := 1
 	maxRange := totalFiles
 	midIndex := minRange + (maxRange-minRange)/2
@@ -83,28 +98,28 @@ func searchFile(mainDir string, totalFiles int, searchedDate time.Time) (int, st
 	for minRange <= maxRange {
 		midIndex = minRange + (maxRange-minRange)/2
 
-		records := readReports(fmt.Sprintf("%s/%d_report.csv", mainDir, midIndex))
-		if records == nil {
-			return 0, ""
+		records, err := readReports(fmt.Sprintf("%s/%d_report.csv", mainDir, midIndex))
+		if err != nil {
+			return 0, "", err
 		}
 
 		firstDate, err := time.Parse(time.RFC3339, records[0][1])
 		if err != nil {
-			fmt.Errorf("invalid date time: %s", err)
-			return 0, ""
+			errMsg := fmt.Errorf("invalid date time: %s", err)
+			return 0, "", errMsg
 		}
 
 		recordLen := len(records)
 		lastDate, err := time.Parse(time.RFC3339, records[recordLen-1][1])
 		if err != nil {
-			fmt.Errorf("invalid date time: %s", err)
-			return 0, ""
+			errMsg := fmt.Errorf("invalid date time: %s", err)
+			return 0, "", errMsg
 		}
 
 		position = checkTimeRange(firstDate, lastDate, searchedDate)
 
 		if position == "equal" {
-			return midIndex, position
+			return midIndex, position, nil
 		} else if position == "left" {
 			maxRange = midIndex - 1
 		} else {
@@ -112,12 +127,12 @@ func searchFile(mainDir string, totalFiles int, searchedDate time.Time) (int, st
 		}
 
 	}
-	return midIndex, position
+	return midIndex, position, nil
 }
 
 // search the index of the searched date within the file. a file contain 20 lines of data.
 // the function will help to search the index/line number where the searched date matched.
-func searchDate(records [][]string, searchedDate time.Time) int {
+func searchDate(records [][]string, searchedDate time.Time) (int, error) {
 	minRange, maxRange := 0, len(records)
 	midIndex := minRange + (maxRange-minRange)/2
 
@@ -126,19 +141,19 @@ func searchDate(records [][]string, searchedDate time.Time) int {
 
 		currentDate, err := time.Parse(time.RFC3339, records[midIndex][1])
 		if err != nil {
-			fmt.Errorf("invalid date time: %s", err)
-			return -1
+			errMsg := fmt.Errorf("invalid date time: %s", err)
+			return -1, errMsg
 		}
 
 		if searchedDate.Equal(currentDate) {
-			return midIndex
+			return midIndex, nil
 		} else if searchedDate.After(currentDate) {
 			minRange = midIndex + 1
 		} else {
 			maxRange = midIndex - 1
 		}
 	}
-	return midIndex
+	return midIndex, nil
 }
 
 func main() {
@@ -163,14 +178,14 @@ func main() {
 	startTime, err := time.Parse(time.RFC3339, startTimeStr)
 
 	if err != nil {
-		fmt.Errorf("invalid date time: %s", err)
+		fmt.Printf("invalid date time: %s\n", err)
 		return
 	}
 
 	endTime, err := time.Parse(time.RFC3339, endTimeStr)
 
 	if err != nil {
-		fmt.Errorf("invalid date time: %s", err)
+		fmt.Printf("invalid date time: %s\n", err)
 		return
 	}
 
@@ -185,51 +200,89 @@ func main() {
 		return
 	}
 
-	fileIndexStart, searchDescStart := searchFile(directory, totalFiles, startTime)
+	fileIndexStart, searchDescStart, err := searchFile(directory, totalFiles, startTime)
 
-	fileIndexEnd, searchDescEnd := searchFile(directory, totalFiles, endTime)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fileIndexEnd, searchDescEnd, err := searchFile(directory, totalFiles, endTime)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	if fileIndexStart == 0 || fileIndexEnd == 0 {
 		fmt.Println("error during search file")
 		return
 	}
 
-	if (searchDescStart == "left" || searchDescEnd == "left") || (searchDescStart == "right" || searchDescEnd == "right") {
+	if (searchDescStart == "left" && searchDescEnd == "left") || (searchDescStart == "right" && searchDescEnd == "right") {
 		fmt.Println("data not found!")
 		return
 	}
 
 	finalReports := make([][]string, 0)
 
-	recordsBegin := readReports(fmt.Sprintf("%s/%d_report.csv", directory, fileIndexStart))
-	startIndex := 0
-	if searchDescStart == "equal" {
-		startIndex = searchDate(recordsBegin, startTime)
+	recordsBegin, err := readReports(fmt.Sprintf("%s/%d_report.csv", directory, fileIndexStart))
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
 
-	recordsEnd := readReports(fmt.Sprintf("%s/%d_report.csv", directory, fileIndexEnd))
+	startIndex := 0
+	if searchDescStart == "equal" {
+		startIndex, err = searchDate(recordsBegin, startTime)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+	}
+
+	recordsEnd, err := readReports(fmt.Sprintf("%s/%d_report.csv", directory, fileIndexEnd))
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 	endIndex := len(recordsEnd) + 1
 	if searchDescEnd == "equal" {
-		endIndex = searchDate(recordsEnd, endTime)
+		endIndex, err = searchDate(recordsEnd, endTime)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
 	}
 
 	isSameFile := fileIndexEnd - fileIndexStart
 
 	if isSameFile == 0 {
-		fmt.Println(startIndex, endIndex)
 		finalReports = append(finalReports, recordsBegin[startIndex:endIndex]...)
 	} else {
 		finalReports = append(finalReports, recordsBegin[startIndex:]...)
 
 		for i := fileIndexStart + 1; i < fileIndexEnd; i++ {
-			records := readReports(fmt.Sprintf("%s/%d_report.csv", directory, i))
-
+			records, err := readReports(fmt.Sprintf("%s/%d_report.csv", directory, i))
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
 			finalReports = append(finalReports, records...)
 		}
 
 		finalReports = append(finalReports, recordsEnd[:endIndex]...)
 	}
 
-	writeReports("final_result", finalReports)
+	err = writeReports("final_result", finalReports)
 
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
